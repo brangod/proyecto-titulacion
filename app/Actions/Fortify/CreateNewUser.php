f14e7913ba3e13a232da4616568a1fd715b8209a
brangod <106929375+brangod@users.noreply.github.com>
2022-06-07 15:53:47 -0500

    Update CreateNewUser.php
    boleta string

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -23,7 +23,7 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'boleta' => ['required', 'integer', 'unique:users'],
+            'boleta' => ['required', 'string', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
